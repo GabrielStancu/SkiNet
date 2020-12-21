dc61db80839464717460be4e7c3b72ce07b821d2
GabrielStancu <gabriel.stancu07@yahoo.com>
2020-12-21 22:22:29 +0200

    End of section 15

--- API/Controllers/BuggyController.cs
+++ API/Controllers/BuggyController.cs
@@ -1,6 +1,7 @@
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using API.Errors;
+using Microsoft.AspNetCore.Authorization;
 
 namespace API.Controllers
 {
@@ -10,6 +11,13 @@ namespace API.Controllers
         public BuggyController(StoreContext context)
         {
             _context = context;
+        }
+
+        [HttpGet("testauth")]
+        [Authorize]
+        public ActionResult<string> GetSecretText()
+        {
+            return "secret stuff";
         }
 
         [HttpGet("notfound")]
